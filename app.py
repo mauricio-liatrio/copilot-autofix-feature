1de084ed7c9ee36bdedfd61576f11cfb9c51f11c
Mauricio <148885544+mauricio-liatrio@users.noreply.github.com>
2024-12-13 11:18:13 -0700

    feat: add bad code

--- app.py
+++ app.py
@@ -17,5 +17,34 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+#
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+      
+        query = f"SELECT * FROM users WHERE username = ? AND password = ?"
+        print(f"Executing query for user: {username}")
+
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute(query)
+        user = cursor.fetchone()
+
+        if user:
+            return f"Welcome, {user[1]}!"
+        else:
+            return "Invalid username or password."
+
+    return '''
+        <form method="post">
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password"><br>
+            <input type="submit" value="Login">
+        </form>
+    '''
+
+
 if __name__ == '__main__':
     app.run(debug=True)
